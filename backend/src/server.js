14b62f442b616470b9d8f0bdda14b0d2ebc2677e
Priyansh <priyans.mehta2003@gmail.com>
2026-01-23 12:20:26 +0530

    DB and Tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import path from "path";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
@@ -17,16 +18,23 @@ app.get("/books", (req, res) => {
 
 //make ready for dployment
 
-if (ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get(/.*/, (req, res) => {
-        res.sendFile(
-            path.join(__dirname, "../frontend/dist/index.html")
-        );
-    });
-}
+app.get(/.*/, (req, res) => {
+    res.sendFile(
+        path.join(__dirname, "../frontend/dist/index.html")
+    );
+});
 
-app.listen(ENV.PORT, () =>
-    console.log(`Server is running on port: ${ENV.PORT}`)
-);
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => {
+        console.log("Server is running on port:", ENV.PORT);
+        });
+    } catch (error) {
+        console.error("Error starting server:", error);
+    }
+};
+
+startServer();
